1cb9cc5d80cde563a4e3b7171e11c6d4856db14f
YousefOsama20 <yousefosama7620@gmail.com>
2026-05-16 16:45:45 +0300

    init_TheProject

---
+++ AIVirtualController/AIVirtualController.py
@@ -0,0 +1,77 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import cv2
+import time
+import numpy as np
+import autopy
+from Lib import library
+# Camera size
+Wcam, Hcam = 640, 480
+
+# Hand detector
+detector = library.HandDetector()
+
+# FPS variable
+pTime = 0
+
+
+def FPS(img, pTime=0):
+
+    cTime = time.time()
+
+    fps = 1 / (cTime - pTime) if pTime != 0 else 0
+
+    cv2.putText(
+        img,
+        f'FPS: {int(fps)}',
+        (10, 70),
+        cv2.FONT_HERSHEY_PLAIN,
+        3,
+        (255, 0, 255),
+        3
+    )
+
+    return cTime
+
+
+# Webcam
+cap = cv2.VideoCapture(0)
+
+cap.set(3, Wcam)
+cap.set(4, Hcam)
+
+
+while True:
+
+    success, img = cap.read()
+    img =cv2.flip( img, 1)
+
+    if not success:
+        break 
+
+    # Detect hand
+    img = detector.findHands(img)
+
+    lmList = detector.findPosition(img)
+    
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1], lmList[8][2]  
+        x2, y2 = lmList[12][1], lmList[12][2]
+
+        fingers = detector.fingersUp()
+
+        print(fingers)
+
+    pTime = FPS(img, pTime)
+
+    # Show image
+    cv2.imshow("Image", img)
+
+    # Quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
